534e1ebdf5e3c6df27023493ec64d1e7699fd1d5
Sahadev1431 <naisahadev@gmail.com>
2024-07-14 15:29:11 +0530

    deploy backend

--- Backend/Controller/userController.js
+++ Backend/Controller/userController.js
@@ -3,153 +3,243 @@ import errorHandler from "../Middleware/errorMiddleware.js";
 import { User } from "../models/userSchema.js";
 import { generateToken } from "../utils/generateToken.js";
 // import moment from 'moment';
-import cloudinary from 'cloudinary'
+import cloudinary from "cloudinary";
 
 export const patientRegister = catchAsyncError(async (req, res, next) => {
-    const { firstName, lastName, email, phone, dob, gender, password, role } = req.body;
+  const { firstName, lastName, email, phone, dob, gender, password, role } =
+    req.body;
 
-    if (!firstName || !lastName || !email || !phone || !dob || !gender || !password || !role) {
-        return next(new errorHandler("Please fill entire form!", 400));
-    }
+  if (
+    !firstName ||
+    !lastName ||
+    !email ||
+    !phone ||
+    !dob ||
+    !gender ||
+    !password ||
+    !role
+  ) {
+    return next(new errorHandler("Please fill entire form!", 400));
+  }
 
-    let user = await User.findOne({ email });
+  let user = await User.findOne({ email });
 
-    if (user) {
-        return next(new errorHandler("User already registered with this email", 400));
-    }
+  if (user) {
+    return next(
+      new errorHandler("User already registered with this email", 400)
+    );
+  }
 
-    // Convert dob to a Date object
-    
+  // Convert dob to a Date object
 
-    user = await User.create({ firstName, lastName, email, phone, dob, gender, password, role });
+  user = await User.create({
+    firstName,
+    lastName,
+    email,
+    phone,
+    dob,
+    gender,
+    password,
+    role,
+  });
 
-    generateToken(user, "User registered!", 200, res)
-
+  generateToken(user, "User registered!", 200, res);
 });
 
 export const login = catchAsyncError(async (req, res, next) => {
-    const { email, password, confirmPassword, role } = req.body
+  const { email, password, confirmPassword, role } = req.body;
 
-    if (!email || !password || !confirmPassword || !role) {
-        return next(new errorHandler("Please Provide all the details!", 400))
-    }
+  if (!email || !password || !confirmPassword || !role) {
+    return next(new errorHandler("Please Provide all the details!", 400));
+  }
 
-    if (password !== confirmPassword) {
-        return next(new errorHandler("Password and confirm password do not match!", 400))
-    }
+  if (password !== confirmPassword) {
+    return next(
+      new errorHandler("Password and confirm password do not match!", 400)
+    );
+  }
 
-    const user = await User.findOne({ email }).select("+password")
+  const user = await User.findOne({ email }).select("+password");
 
-    if (!user) {
-        return next(new errorHandler("Invalid email or password!", 400))
-    }
-    const isPasswordMatch = await user.comparePassword(password)
-    if (!isPasswordMatch) {
-        return next(new errorHandler("Invalid email or password!", 400))
-    }
+  if (!user) {
+    return next(new errorHandler("Invalid email or password!", 400));
+  }
+  const isPasswordMatch = await user.comparePassword(password);
+  if (!isPasswordMatch) {
+    return next(new errorHandler("Invalid email or password!", 400));
+  }
 
-    if (role !== user.role) {
-        return next(new errorHandler(`Only ${role} can login!`, 400))
-    }
+  if (role !== user.role) {
+    return next(new errorHandler(`Only ${role} can login!`, 400));
+  }
 
-    generateToken(user, `${user.role} logged in successfully!`, 200, res)
-})
+  generateToken(user, `${user.role} logged in successfully!`, 200, res);
+});
 
 export const newAdmin = catchAsyncError(async (req, res, next) => {
-    const { firstName, lastName, email, phone, dob, gender, password } = req.body;
+  const { firstName, lastName, email, phone, dob, gender, password } = req.body;
 
-    if (!firstName || !lastName || !email || !phone || !dob || !gender || !password) {
-        return next(new errorHandler("Please fill entire form!", 400));
-    }
+  if (
+    !firstName ||
+    !lastName ||
+    !email ||
+    !phone ||
+    !dob ||
+    !gender ||
+    !password
+  ) {
+    return next(new errorHandler("Please fill entire form!", 400));
+  }
 
-    const isRegisterd = await User.findOne({ email })
-    if (isRegisterd) {
-        return next(new errorHandler(`${isRegisterd.role} with this Email already exists!`, 400))
-    }
+  const isRegisterd = await User.findOne({ email });
+  if (isRegisterd) {
+    return next(
+      new errorHandler(
+        `${isRegisterd.role} with this Email already exists!`,
+        400
+      )
+    );
+  }
 
-    
-
-    const admin = await User.create({ firstName, lastName, email, phone, dob, gender, password, role: "Admin" })
-    res.status(200).json({
-        success: true,
-        message: "New Admin registered"
-    })
-})
+  const admin = await User.create({
+    firstName,
+    lastName,
+    email,
+    phone,
+    dob,
+    gender,
+    password,
+    role: "Admin",
+  });
+  res.status(200).json({
+    success: true,
+    message: "New Admin registered",
+  });
+});
 
 export const getAllDoctors = catchAsyncError(async (req, res, next) => {
-    const doctors = await User.find({ role: "Doctor" })
-    res.status(200).json({
-        success: true,
-        doctors
-    })
-})
+  const doctors = await User.find({ role: "Doctor" });
+  res.status(200).json({
+    success: true,
+    doctors,
+  });
+});
 
 export const getUserDetail = catchAsyncError(async (req, res, next) => {
-    const user = req.user
-    res.status(200).json({
-        success: true,
-        user
-    })
-})
+  const user = req.user;
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
 
 export const logoutAdmin = catchAsyncError(async (req, res, next) => {
-    res.status(200).cookie("adminToken", "", {
-        httpOnly: true,
-        expires: new Date(Date.now())
-    }).json({
-        success: true,
-        message: "Admin logged out successfully!"
+  res
+    .status(200)
+    .cookie("adminToken", "", {
+      httpOnly: true,
+      expires: new Date(Date.now()),
+      secure: true,
+      sameSite: "None",
     })
-})
+    .json({
+      success: true,
+      message: "Admin logged out successfully!",
+    });
+});
 
 export const logoutPatient = catchAsyncError(async (req, res, next) => {
-    res.status(200).cookie("patientToken", "", {
-        httpOnly: true,
-        expires: new Date(Date.now())
-    }).json({
-        success: true,
-        message: "Patient logged out successfully!"
+  res
+    .status(200)
+    .cookie("patientToken", "", {
+      httpOnly: true,
+      expires: new Date(Date.now()),
+      sameSite: "None",
+      secure: true,
     })
-})
+    .json({
+      success: true,
+      message: "Patient logged out successfully!",
+    });
+});
 
 export const addNewDoctor = catchAsyncError(async (req, res, next) => {
-    if (!req.files || Object.keys(req.files).length === 0) {
-        return next(new errorHandler("Doctor Avatar required"))
-    }
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return next(new errorHandler("Doctor Avatar required"));
+  }
 
-    const { docAvatar } = req.files
-    const allowedFormats = ["image/png", "image/jpeg", "image/webp", "images/jpg"]
+  const { docAvatar } = req.files;
+  const allowedFormats = [
+    "image/png",
+    "image/jpeg",
+    "image/webp",
+    "images/jpg",
+  ];
 
-    if (!allowedFormats.includes(docAvatar.mimetype)) {
-        return next(new errorHandler("File Format not Supported!", 400))
-    }
-    const { firstName, lastName, email, phone, dob, gender, password, doctorDepartment } = req.body
+  if (!allowedFormats.includes(docAvatar.mimetype)) {
+    return next(new errorHandler("File Format not Supported!", 400));
+  }
+  const {
+    firstName,
+    lastName,
+    email,
+    phone,
+    dob,
+    gender,
+    password,
+    doctorDepartment,
+  } = req.body;
 
-    if (!firstName || !lastName || !email || !phone || !dob || !gender || !password || !doctorDepartment) {
-        return next(new errorHandler("Please fill entire form!", 400));
-    }
+  if (
+    !firstName ||
+    !lastName ||
+    !email ||
+    !phone ||
+    !dob ||
+    !gender ||
+    !password ||
+    !doctorDepartment
+  ) {
+    return next(new errorHandler("Please fill entire form!", 400));
+  }
 
-    const isRegisterd = await User.findOne({ email })
-    if (isRegisterd) {
-        return next(new errorHandler(`${isRegisterd.role} already registered with this Email`))
-    }
+  const isRegisterd = await User.findOne({ email });
+  if (isRegisterd) {
+    return next(
+      new errorHandler(`${isRegisterd.role} already registered with this Email`)
+    );
+  }
 
-    const cloudinaryResponse = await cloudinary.uploader.upload(docAvatar.tempFilePath)
+  const cloudinaryResponse = await cloudinary.uploader.upload(
+    docAvatar.tempFilePath
+  );
 
-    if (!cloudinaryResponse || cloudinaryResponse.error) {
-        console.error("Cloudinary Error:",cloudinaryResponse.error || "unknown cloudinary error")
-    }
+  if (!cloudinaryResponse || cloudinaryResponse.error) {
+    console.error(
+      "Cloudinary Error:",
+      cloudinaryResponse.error || "unknown cloudinary error"
+    );
+  }
 
-
-    const doctor = await User.create( { firstName, lastName, email, phone, dob , gender, password, doctorDepartment, role : "Doctor",
-        docAvatar : {
-        public_id : cloudinaryResponse.public_id,
-        url : cloudinaryResponse.secure_url
-
-    } } )
-    res.status(200).json({
-        success : true,
-        message : "New Doctor Registered!",
-        doctor
-    })
-})
+  const doctor = await User.create({
+    firstName,
+    lastName,
+    email,
+    phone,
+    dob,
+    gender,
+    password,
+    doctorDepartment,
+    role: "Doctor",
+    docAvatar: {
+      public_id: cloudinaryResponse.public_id,
+      url: cloudinaryResponse.secure_url,
+    },
+  });
+  res.status(200).json({
+    success: true,
+    message: "New Doctor Registered!",
+    doctor,
+  });
+});
+ 
--- Backend/database/dbConnection.js
+++ Backend/database/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnection = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI,{
-            dbName : "HOSPITAL_MANAGEMENT_SYSTEM_MERN"
+            dbName : "HOSPITAL_MANAGEMENT_SYSTEM_MERN_DEPLOY"
         })
         console.log("Database connected successfully");
     } catch (err) {
